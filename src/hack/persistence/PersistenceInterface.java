8a5b56122629bc88b94e6dd41e3584a4d815c445
Rahul Malviya <malviyarahul2001@gmail.com>
2013-09-28 14:43:19 -0700

    Persistence layer.

---
+++ src/hack/persistence/PersistenceInterface.java
@@ -0,0 +1,11 @@
+package hack.persistence;
+
+/**
+ * This class holds the interface will be used to implement interaction 
+ * @author rahul
+ *
+ */
+public interface PersistenceInterface 
+{
+
+}
